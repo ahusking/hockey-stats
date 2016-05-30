641070229ead67c8c1d3a955d4479105e0c224bf
Andrew husking <Andrew@husking.id.au>
2016-05-30 14:41:47 +1000

    Cleapup v2

--- buildclubstatsjson.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("hockeyfunctions.php");
-$Response = EnumerateCompetitions(GetCompetitionList());
-$myfile = fopen("unitedcomp.json", "w");
-
-fwrite($myfile, json_encode($compJSON));
-?>
--- generateclubxls.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("hockeyfunctions.php");
-$compJSON = json_decode(file_get_contents ("unitedcomp.json"),1);
-GenerateClubResultsXLS($compJSON);
-?>
